5a89f1c03770d3b1d3bc9aa208f36b5de31fafeb
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-08-15 16:34:23 -0700

    Added Daily Report project

---
+++ DailyReport/DailyReport/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyReport
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // introduction
+            Console.WriteLine("The Tech Academy\nStudent Daily Report");
+
+            // prompts student for name and saves response as string var
+            Console.WriteLine("What's your name?");
+            string studentName = Console.ReadLine();
+
+            // prompts student for course and saves response as string var
+            Console.WriteLine("What course are you on?");
+            string studentCourse = Console.ReadLine();
+
+            // prompts student for page number and saves response as int var
+            // by parsing string 
+            Console.WriteLine("What page number are you on?");
+            int pageNumber = int.Parse(Console.ReadLine());
+
+            // prompts student for help and casts response as boolean var
+            Console.WriteLine("Do you need  help with anything? Please answer true or false.");
+            string needHelp = Console.ReadLine();
+            bool needHelpBool = Convert.ToBoolean(needHelp);
+
+            // prompts student for experiences and saves response as string var
+            Console.WriteLine("Were there any positive experiences you'd like to share?");
+            string experiences = Console.ReadLine();
+
+            // prompts student for feedback and saves response as string var
+            Console.WriteLine("Is there any other feedback you'd like to provide?");
+            string feedback = Console.ReadLine();
+
+            // prompts student for study hours and saves response as int var
+            Console.WriteLine("How many hours did you study today?");
+            int hoursStudied = int.Parse(Console.ReadLine());
+
+            // prints thank you message
+            Console.WriteLine("Thank you for your answers. An instructor will respond to this shortly. Have a great day!");
+
+
+        }
+    }
+}
---
+++ VariablesAndDataTypes/VariablesAndDataTypes/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VariablesAndDataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // this is a simple program
+            Console.WriteLine("What's your name?");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Your name is " + yourName);
+            Console.ReadLine();
+
+            Console.WriteLine("What's your favorite number?");
+            string faveNumber = Console.ReadLine();
+            Console.WriteLine("Your fave num is " + faveNumber);
+            Console.ReadLine();
+
+            bool isStudying = false;
+            byte hoursWorked = 42;
+            sbyte currentTemp = -23;
+            char randomLetter = '\u2103';
+
+            decimal moneyInBacnk = 100.5m;
+            double heightInCm = 211.23498230;
+            float secondsLeft = 2.62f;
+            short tempOnMars = -341;
+            string myName = "Carter";
+
+            int currentAge = 30;
+            string yearsOld = currentAge.ToString();
+            
+
+
+        }
+    }
+}
