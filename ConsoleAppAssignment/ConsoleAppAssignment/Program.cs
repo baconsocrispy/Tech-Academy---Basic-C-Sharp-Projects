45630d3a117ccd003accc9525c454b8f50374c61
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-08-15 17:03:28 -0700

    Added ConsoleAppAssignment

---
+++ ConsoleAppAssignment/ConsoleAppAssignment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // take user input and multiply by 50, print to console
+            Console.WriteLine("Pick a number less than 10m");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1 * 50);
+
+            // adds 25 to user input, prints to console
+            Console.WriteLine("Pick another number");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num2 + 25);
+
+            // divides user input by 12.5, prints to console
+            Console.WriteLine("Pick another number");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num3 / 12.5);
+
+            // tests if user input greater than 50, prints to console
+            Console.WriteLine("Pick another number");
+            int num4 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num4 > 50);
+
+            // divides user input by 7, prints remainder to console
+            Console.WriteLine("Pick another number");
+            int num5 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num5 % 7);
+
+            Console.Read();
+        }
+    }
+}
---
+++ MathChallenge/MathChallenge/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathChallenge
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int num1 = 25;
+            int num2 = 25;
+            int sumTotal = num1 + num2;
+
+            int num3 = 65;
+            int num4 = 32;
+            int difference = num3 - num4;
+
+            int num5 = 984375;
+            int num6 = 14;
+            int product = num5 * num6;
+
+            int num7 = 239487;
+            int num8 = 31;
+            int quotient = num7 / num8;
+
+            Console.WriteLine(sumTotal);
+            Console.WriteLine(difference);
+            Console.WriteLine(product);
+            Console.WriteLine(quotient);
+         
+            string fname = "Carter";
+            string lname = "Bacon";
+            string name = fname + lname;
+
+            Console.WriteLine(name);
+
+
+            Console.Read();
+
+        }
+    }
+}
