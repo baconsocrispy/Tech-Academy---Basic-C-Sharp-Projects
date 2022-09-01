373d5b17862ddbf521c67d08d2fd47b7408c070d
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-09-01 14:08:50 -0700

    Added iteration video exercise

---
+++ IterationVideo/IterationVideo/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationVideo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] testScores = { 98, 99, 85, 70, 82, 34, 91, 90, 94 };
+
+            for (int i = 0; i < testScores.Length; i++)
+            {
+                if (testScores[i] > 85)
+                {
+                    Console.WriteLine("Passing test score: " + testScores[i]);
+                }
+            }
+
+            string[] names = { "Jesse", "Erik", "David", "Beth" };
+
+            for (int i = 0; i < names.Length; i++)
+            {
+                if (names[i] == "David")
+                {
+                    Console.WriteLine("We found " + names[i]);
+                }
+            }
+
+            List<int> testScores1 = new List<int>();
+            testScores1.Add(98);
+            testScores1.Add(99);
+            testScores1.Add(81);
+            testScores1.Add(72);
+            testScores1.Add(70);
+
+            foreach (int score in testScores1)
+            {
+                if (score > 85)
+                {
+                    Console.WriteLine("Passing test score: " + score);
+                }
+            }
+
+            List<string> names1 = new List<string>() { "Jesse", "Erik", "David", "Beth" };
+
+            foreach (string name in names1)
+            {
+                Console.WriteLine(name);
+            }
+
+            List<int> testScores2 = new List<int>() { 98, 99, 12, 74, 23, 99 };
+            List<int> passingScores = new List<int>();
+
+            foreach (int score in testScores2)
+            {
+                if (score > 85)
+                {
+                    passingScores.Add(score);
+                }
+            }
+            Console.WriteLine(passingScores.Count);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ StringAssignment/StringAssignment/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringAssignment
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            // concatenate 3 strings
+            string first = "What ";
+            string second = "time ";
+            string third = "is it???";
+            string together = first + second + third;
+            Console.WriteLine(together);
+
+            // convert a string to uppercase
+            Console.WriteLine(together.ToUpper());
+
+            // create a Stringbuilder and build a paragraph of text, one sentence at a time
+            string sentence1 = "Johnny was three years old when his back started to ache, as if an old man. ";
+            string sentence2 = "He went to the doctor, who declared that Johnny was not three, but in fact 103. ";
+            string sentence3 = "Johnny had forgotten how old he was. ";
+            string sentence4 = "His death 3 days later was welcomed by all who knew him. ";
+            string sentence5 = "It's true that Johnny was horrible. ";
+
+            StringBuilder sb = new StringBuilder();
+            sb.Append(sentence1);
+            sb.Append(sentence2);
+            sb.Append(sentence3);
+            sb.Append(sentence4);
+            sb.Append(sentence5);
+
+            Console.WriteLine(sb);
+
+            Console.Read();
+        }
+    }
+}
