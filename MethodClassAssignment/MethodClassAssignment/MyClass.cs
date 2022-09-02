299c84b84a550d7e4e279e5a440a66ff8e43bfa7
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-09-02 12:39:37 -0700

    Added MethodClassAssignment

---
+++ MethodClassAssignment/MethodClassAssignment/MyClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    internal class MyClass
+    {
+        // create a class
+        public MyClass() { }
+
+
+        //create a void method that takes two int params
+        public void Math(int num1, int num2)
+        {
+            // have the method do a math operation on the first int
+            // and display the second int to screen
+            num1 *= 5000;
+            Console.WriteLine(num2);
+        }
+    }
+}
---
+++ MethodClassAssignment/MethodClassAssignment/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodClassAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the class
+            MyClass test_object = new MyClass();
+
+            // call the class method, passing in 2 numbers
+            test_object.Math(32, 52);
+
+            // call the class method, specifying the params by name
+            test_object.Math(num1: -43, num2: -56);
+
+            Console.ReadLine();
+        }
+    }
+}
