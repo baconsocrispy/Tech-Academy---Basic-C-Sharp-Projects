654ca9201b644180ef2a140d2800c83a93080d72
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-09-02 12:26:17 -0700

    Added MethodSubmittionAssignment

---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/MyClass.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    internal class MyClass
+    {
+        // create a class
+        public MyClass() { }
+
+        // create a method that takes 2 int parameters with one being optional
+        public int Math(int num1, int num2 = 1)
+        {
+            return num1 * num2;
+        }
+    }
+}
---
+++ MethodSubmissionAssignment/MethodSubmissionAssignment/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodSubmissionAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the class
+            MyClass class_object = new MyClass();
+
+            // prompt user for 2 numbers
+            Console.WriteLine("Choose a number:");
+            int num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Choose a number....or don't:");
+            string num2 = Console.ReadLine();
+            
+            // if a second number is chosen, run  method with both
+            if (int.TryParse(num2, out int value))
+            {
+                int num3 = value;
+                Console.WriteLine(class_object.Math(num1, num3));
+            }
+            else
+            {
+                // if no second number, run method with one number
+                Console.WriteLine(class_object.Math(num1));
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
