0952a94e6138bfed5497ceb97f8ff391072bfc3b
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-09-02 11:32:56 -0700

    Added CallingMethodsAssignment

---
+++ CallingMethodsAssignment/CallingMethodsAssignment/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            YourNum num1 = new YourNum(15);
+
+            int result1 = num1.Multiply(32);
+            int result2 = num1.Add(345879);
+            int result3 = num1.Subtract(23179487);
+
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+            Console.WriteLine(result3);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CallingMethodsAssignment/CallingMethodsAssignment/YourNum.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallingMethodsAssignment
+{   
+    // Create a class w/3 methods that each take one integer
+    // parameter in  and return an integer. Methods should
+    // do some math operations.
+    internal class YourNum
+    {
+        // Constructor method. Sets user Num parameter
+        // on construction
+        public YourNum(int Num1) 
+        {
+            Num = Num1;
+        }
+
+        // Creates get/set methods to access/change
+        // user number
+        public int Num { get; set; } 
+
+        // Multiplies user number by a chosen multiplier
+        public int Multiply(int multiplier)
+        {
+            return this.Num * multiplier;
+        }
+
+        // Adds a chosen number to the user number
+        public int Add(int num2)
+        {
+            return this.Num + num2;
+        }
+
+        // Subtracts a chosen number from the user number
+        public int Subtract(int num3)
+        {
+            return this.Num - num3;
+        }
+    }
+}
