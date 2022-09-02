60b1ed8008bfd8159c966de08dee97ed111357dd
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-09-02 12:08:15 -0700

    Added MainMethodAssignment

---
+++ MainMethodAssignment/MainMethodAssignment/MathClass.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class MathClass
+    {
+        // create a class
+        public MathClass(int num1)
+        {
+            Num = num1;
+        }
+        public int Num { get; set; }
+
+        // create a method that takes in an integer,
+        // does some math and returns an integer
+        public int Multiply(int num2)
+        {
+            return this.Num * num2;
+        }
+
+        // create a different method with the same name that takes a decimal
+        // Create a different math operation and return answer as integer.
+        public int Multiply(decimal num3)
+        {
+            int n = Convert.ToInt32(num3);
+            return this.Num * 2 * n;
+        }
+
+        // add a third method with the same name that takes a string,
+        // converts it to an integer if possible. Does a different math operation.
+        public int Multiply(string num4)
+        {
+            int n = int.Parse(num4);
+            return this.Num * -4000 * n;
+        }
+    }
+}
---
+++ MainMethodAssignment/MainMethodAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainMethodAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // instantiate the class
+            MathClass new_class = new MathClass(25);
+
+            // call the method and print result to screen
+            Console.WriteLine(new_class.Multiply(25));
+
+            // instantiate the class
+            MathClass another_class = new MathClass(62);
+
+            // call the second method and print to screen
+            Console.WriteLine(another_class.Multiply(44.7623m));
+
+            // instantiate the class
+            MathClass yet_another_class = new MathClass(19);
+
+            // call the third method and print to screen
+            Console.WriteLine(yet_another_class.Multiply("-6"));
+
+            Console.ReadLine();
+        }
+    }
+}
