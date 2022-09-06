c8f0a11396493ac18bb7bf7d71f58b4df66e79a7
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-09-06 11:10:39 -0700

    Added PolyMorphismAssignment

---
+++ PolyMorphismAssignment/PolyMorphismAssignment/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorphismAssignment
+{
+    internal class Employee : Person, IQuittable
+    {
+        public int Id { get; set; }
+
+        public override void SayName()
+        {
+            Console.WriteLine("Name: " + FirstName + " " + LastName);
+        }
+
+        // implement the Quit() method in any way you choose
+        public void Quit()
+        {
+            Console.WriteLine("I quit, gosh darn it!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ PolyMorphismAssignment/PolyMorphismAssignment/IQuittable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorphismAssignment
+{
+    // create an interface call IQuitable
+    internal interface IQuittable
+    {
+        // define a void method called quit
+        void Quit();
+    }
+}
