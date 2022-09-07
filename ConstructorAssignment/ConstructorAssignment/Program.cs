547c4dc000bf81d9022c8dc2b61f744537a917c9
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-09-07 11:30:20 -0700

    Added ConstructorAssignment

---
+++ ConstructorAssignment/ConstructorAssignment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // create a const variable
+            const string sky_color = "GREEN";
+
+            //// create a variable using var
+            var sky_string = new SkyClass(14, sky_color);
+
+            Console.WriteLine(sky_string.Clouds);
+            Console.WriteLine(sky_string.SkyColor);
+
+            Console.Read();
+
+        }
+    }
+}
---
+++ ConstructorAssignment/ConstructorAssignment/SkyClass.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstructorAssignment
+{
+    internal class SkyClass
+    {
+        // chain two constructors together
+        public SkyClass(int clouds) : this(clouds, "BLUE")
+        {
+        }
+        public SkyClass(int clouds, string sky_color)
+        {
+            Clouds = clouds;
+            SkyColor = sky_color;
+        }
+        public string SkyColor { get; set; }
+        public int Clouds { get; set; }
+    }
+}
