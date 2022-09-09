52a40eace2f81eccacaf1b4a99a6d1acd1903e49
baconsocrispy <68571497+baconsocrispy@users.noreply.github.com>
2022-09-09 11:26:39 -0700

    Added EFCodeFirstAssignment

---
+++ CoreTourVideo/CoreTourVideo/Controllers/Products.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CoreTourVideo.Controllers
+{
+    public class Products : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ EFCodeFirstAssignment/EFCodeFirstAssignment/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFCodeFirstAssignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var ctx = new SchoolContext())
+            {
+                var stud = new Student() { StudentName = "Bill" };
+                ctx.Students.Add(stud);
+                ctx.SaveChanges();
+            }
+        }
+    }
+}
